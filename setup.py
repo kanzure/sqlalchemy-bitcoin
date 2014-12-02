3d141313738acf1e19cc1248bb0becd473fcc8b0
Bryan Bishop <kanzure@gmail.com>
2014-12-02 12:11:19 -0600

    don't filter requires
    This prevents an error in setuptools:
        TypeError: object of type 'filter' has no len()

--- setup.py
+++ setup.py
@@ -8,8 +8,7 @@ from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.md')).read()
-requires = filter(lambda r:'libs/' not in r,
-    open(os.path.join(here, 'requirements.txt')).read().split())
+requires = open(os.path.join(here, 'requirements.txt')).read().split()
 packages = filter(lambda p:not p.startswith('xunit'), find_packages())
 
 version = '0.0.3pre-alpha'
